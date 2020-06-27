2b23668a4bb68f18427086e3f5ac8ce1c3091236
Ernest Oppetit <eoppetit@gmail.com>
2020-06-27 23:28:50 +0000

    correctly reading reply tweet, extracting url, regexing

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 
 import os
-import oauth2 as oauth
 from requests_oauthlib import OAuth1Session
 import json
+import re
 
 def load_env(): 
   from dotenv import load_dotenv
   load_dotenv()
 
 def get_api_client():
-  consumer = oauth.Consumer(key=os.getenv("CONSUMER_KEY"), secret=os.getenv("CONSUMER_SECRET"))
-  access_token = oauth.Token(key=os.getenv("ACCESS_TOKEN"), secret=os.getenv("ACCESS_TOKEN_SECRET"))
-  client = oauth.Client(consumer, access_token)
-  print(client)
-  return client
 
-def get_api_client2():
   oauth = OAuth1Session(os.getenv("CONSUMER_KEY"),
                        client_secret=os.getenv("CONSUMER_SECRET"),
                        resource_owner_key=os.getenv("ACCESS_TOKEN"),
@@ -24,66 +18,87 @@ def get_api_client2():
   return oauth
 
 def get_mentions(client, last_mention_id):
-  # https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-mentions_timeline
-  # mentions_endpoint = ""
-  # params  = '?trim_user=1,since_id=' + str(last_mention_id)
-  # mentions_endpoint += params
 
-  #TODO Error handling on request
-  # response, data = client.request(mentions_endpoint)
-
+  mentions_endpoint = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json'
   params = {"trim_user": "1", "since_id" : str(last_mention_id)}
 
-  response = client.get("https://api.twitter.com/1.1/statuses/mentions_timeline.json", params = params)
+  #TODO error handling
+  response = client.get(mentions_endpoint, params = params)
 
-  print('MENTIONS RESPONSE')
-  print (response.text)
-
-  return response
+  return json.loads(response.content)
 
 def get_tweet(client, id):
 
-  # statuses_endpoint = "https://api.twitter.com/labs/2/tweets/1276992893970833408"
-  # params =  str(id) + '&tweet.fields=text,created_at,attachments,entities'
-  # statuses_endpoint += params
-
-  # statuses_endpoint = "https://api.twitter.com/labs/2/tweets.json?"
-  # params =  'ids=' + str(id) + '&tweet.fields=text,created_at,attachments,entities'
-  # statuses_endpoint += params
+  statuses_endpoint = "https://api.twitter.com/labs/2/tweets"
   
   params = {"ids": "1276881317557415937", "tweet.fields": "text,created_at,attachments,entities", "media.fields": "url", "expansions": "attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id"}
 
-  response = client.get("https://api.twitter.com/labs/2/tweets", params = params)
-
-  print(response.text)
-
-  # print(statuses_endpoint)
-
   #TODO error handling
-  # response, data = client.request(statuses_endpoint)
+  response = client.get(statuses_endpoint, params = params)
 
-  # print(data)
-  # return json.loads(data)
+  return json.loads(response.content)
+
+def transform_amazon_url(url):
+    print('aiming to transform url ' + url )
+
+    # https://regex101.com/
+    regex = r"(https?:\/\/(.+?\.)?amazon\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)"
+    matches = re.match(regex, url)
+
+    if(matches):
+      transformed_url = url.replace('amazon', 'smile.amazon', 1)
+      return {"success": True, "transformed_url": transformed_url}
+    else:
+      transformed_url = ''
+      return {"success": False, "transformed_url": transformed_url}
 
 def main():
   load_env()
-  client = get_api_client2()
+  client = get_api_client()
 
-  t = get_tweet(client, 1276992893970833408)
-
-  print (t)
-
-  # TODO: Save the last m?ention ID in file
+  # TODO: Save the last mention ID in file
   mentions = get_mentions(client,1)
 
-  # for mention in mentions:
-    # print("dealing with mention " + mention['id_str'])
-    # print(mention)
+  for mention in mentions:
+    print("---- Mention is ID: " + mention['id_str'])
+    print(mention)
     
-  #   reply_id = mention['in_reply_to_status_id']
+    reply_id = mention['in_reply_to_status_id_str']
+    
+    if(reply_id):
+    
+      #MENTION IS IN REPLY TO A TWEET
+    
+      print("--------- found a reply ID " + reply_id)
 
-  #   if(reply_id):
-  #     get_tweet(client, reply_id)
+      replied_tweet = get_tweet(client, reply_id)
+      print("--------------- replied to tweet is  ")
+      print(replied_tweet)
+      
+      urls = replied_tweet['data'][0]['entities']['urls'] 
+
+      if (urls):  
+
+        for url in urls:
+
+          transformed_url = transform_amazon_url(url['unwound_url'])
+          
+          if(transformed_url['success']):
+            print('matched amazon url')
+            print('new smile url is ' + transformed_url['transformed_url'])
+            # TODO send the smile tweet back 
+
+          else:
+            print('couldnt match amazon url in this tweet')
+      
+      else:
+        print('no amazon url found in tweet')
+        #TODO generic tweet
+
+    else:
+      print('mention without a reply')
+      #TODO generic tweet
 
 if __name__ == "__main__":
     main()
+    # print(transform_amazon_url('https://www.amazon.com/dp/1442265639/ref=cm_sw_r_cp_api_i_ze19Eb5DRNN42'))
