fad527810928e06f7c9099c7e6d2af4c7adee969
Ernest Oppetit <eoppetit@gmail.com>
2020-06-28 00:34:46 +0000

    Happy path working

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_api_client():
 def get_mentions(client, last_mention_id):
 
   mentions_endpoint = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json'
-  params = {"trim_user": "1", "since_id" : str(last_mention_id)}
+  params = {"since_id" : str(last_mention_id)}
 
   #TODO error handling
   response = client.get(mentions_endpoint, params = params)
@@ -31,12 +31,21 @@ def get_tweet(client, id):
 
   statuses_endpoint = "https://api.twitter.com/labs/2/tweets"
   
-  params = {"ids": "1276881317557415937", "tweet.fields": "text,created_at,attachments,entities", "media.fields": "url", "expansions": "attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id"}
+  params = {"ids": id, "tweet.fields": "text,created_at,attachments,entities", "media.fields": "url", "expansions": "attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id", "user.fields" : "username"}
 
   #TODO error handling
   response = client.get(statuses_endpoint, params = params)
 
   return json.loads(response.content)
+
+def send_tweet(client, text, in_reply_to_status_id):
+  send_tweet_endpoint = "https://api.twitter.com/1.1/statuses/update.json"
+  params = {"status": text, "in_reply_to_status_id": in_reply_to_status_id}
+
+  response = client.post(send_tweet_endpoint, params = params)
+
+  print(response.content)
+  # return json.loads(response.content)
 
 def transform_amazon_url(url):
     print('aiming to transform url ' + url )
@@ -46,36 +55,39 @@ def transform_amazon_url(url):
     matches = re.match(regex, url)
 
     if(matches):
-      transformed_url = url.replace('amazon', 'smile.amazon', 1)
+      transformed_url = url.replace('www.amazon', 'smile.amazon', 1)
       return {"success": True, "transformed_url": transformed_url}
     else:
       transformed_url = ''
       return {"success": False, "transformed_url": transformed_url}
 
 def main():
-  load_env()
-  client = get_api_client()
 
   # TODO: Save the last mention ID in file
   mentions = get_mentions(client,1)
 
   for mention in mentions:
-    print("---- Mention is ID: " + mention['id_str'])
-    print(mention)
+    mention_id = mention['id_str']
+    print("---- PROCESSING MENTION ID: " + mention_id )
+    username =  mention['user']['screen_name']
+    print("------- username that wrote mention is : " + username )
+    print("------- mention text" + mention['text'])
     
-    reply_id = mention['in_reply_to_status_id_str']
+    original_tweet_id = mention['in_reply_to_status_id_str']
     
-    if(reply_id):
+    if(original_tweet_id):
     
       #MENTION IS IN REPLY TO A TWEET
     
-      print("--------- found a reply ID " + reply_id)
+      print("-------------- found an original tweet id  " + original_tweet_id)
 
-      replied_tweet = get_tweet(client, reply_id)
-      print("--------------- replied to tweet is  ")
+      replied_tweet = get_tweet(client, original_tweet_id)
+      print("-------------- the original tweet is  ")
       print(replied_tweet)
       
       urls = replied_tweet['data'][0]['entities']['urls'] 
+      #wrong username to reply to
+      # username = replied_tweet['includes']['users'][0]['username']
 
       if (urls):  
 
@@ -84,21 +96,33 @@ def main():
           transformed_url = transform_amazon_url(url['unwound_url'])
           
           if(transformed_url['success']):
-            print('matched amazon url')
-            print('new smile url is ' + transformed_url['transformed_url'])
+            print('----------------------------matched amazon url')
+            print('----------------------------new smile url is ' + transformed_url['transformed_url'])
             # TODO send the smile tweet back 
 
+            text = "Hey @" + username + " here is your smile link: " +  transformed_url['transformed_url']
+            send_tweet(client, text, mention_id)
+
           else:
-            print('couldnt match amazon url in this tweet')
+            print('--------------couldnt match amazon url in original tweet')
       
       else:
-        print('no amazon url found in tweet')
+        print('--------------no amazon url found in original tweet')
         #TODO generic tweet
 
     else:
-      print('mention without a reply')
+      print('-------mention was not a reply to a tweet')
+      #TODO check if there is a url in the mention, if so transform it and return
       #TODO generic tweet
 
 if __name__ == "__main__":
+    load_env()
+    client = get_api_client()
     main()
+
+    # send_tweet(client,"@ernopp testingasdasd123 https://www.smile.amazon.com/dp/1442265639/ref=cm_sw_r_cp_api_i_ze19Eb5DRNN42", '1273945165217124352')
+
     # print(transform_amazon_url('https://www.amazon.com/dp/1442265639/ref=cm_sw_r_cp_api_i_ze19Eb5DRNN42'))
+
+    # t = get_tweet(client, '1276631955132407808')
+    # print(t)
