f28ede085ff489ceea963211579ba63cb12ff45d
Ernest Oppetit <ernest@improbable.io>
2020-07-18 14:23:01 +0100

    again

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_mentions(client, last_mention_id):
 
   response = client.get(mentions_endpoint, params = params)
 
-  logging.debug('mentions api call response ' + str(response))
+  logging.debug('mentions api call response ' + str(json.loads(response)))
 
   return json.loads(response.content)
 
